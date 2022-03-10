930b198c0848c89d77a5cd51457b4925f1b6461e
todinesh <33295255+todinesh@users.noreply.github.com>
2022-03-10 08:58:51 -0500

    Initial commit

---
+++ src/main/java/com/dishatech/eureka/discovery/MyDemoEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.dishatech.eureka.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MyDemoEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDemoEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dishatech/eureka/discovery/MyDemoEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dishatech.eureka.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDemoEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
